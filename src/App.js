9030ec2d104ac53424970bb0176420e035dd1edc
bpunam967 <you@example.com>
2022-04-09 21:02:30 +0545

    First time Add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Carousel from "./component/Carousel";
+import Navbar from "./component/Navbar";
+
+
+function App() {
+  return (
+   <>
+    <Navbar/>
+    <Carousel/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/component/Carousel.js
@@ -0,0 +1,135 @@
+import React from 'react'
+
+function Carousel() {
+  return (
+    <div>
+<section id="gtco-counter" class="overlay bg-fixed">
+    <div class="container">
+        <div class="section-content">
+            <div class="row">
+                {/* <!-- Counter Item --> */}
+                <div class="col-md-3 col-sm-6 counter-item">
+                    <i class="lnr lnr-users"></i>
+                    <span class="number" data-from="0" data-to="34" data-refresh-interval="100">14</span>
+                    <h4>Clients</h4>
+                </div>
+                {/* <!-- End of Counter Item -->
+                <!-- Counter Item --> */}
+                <div class="col-md-3 col-sm-6 counter-item">
+                    <i class="lnr lnr-briefcase"></i>
+                    <span class="number" data-from="0" data-to="32" data-refresh-interval="100">32</span>
+                    <h4>Projects</h4>
+                </div>
+                {/* <!-- End of Counter Item -->
+                <!-- Counter Item --> */}
+                <div class="col-md-3 col-sm-6 counter-item">
+                    <i class="lnr lnr-heart"></i>
+                    <span class="number" data-from="0" data-to="38" data-refresh-interval="100">38</span>
+                    <h4>Positive Feedbacks</h4>
+                </div>
+                {/* <!-- End of Counter Item --> */}
+                {/* <!-- Counter Item --> */}
+                <div class="col-md-3 col-sm-6 counter-item">
+                    <i class="lnr lnr-rocket"></i>
+                    <span class="number" data-from="0" data-to="29" data-refresh-interval="100">29</span>
+                    <h4>Website Launch</h4>
+                </div>
+                {/* <!-- End of Counter Item --> */}
+            </div>
+        </div>
+    </div>
+</section>
+{/* <!-- End of Counter Section -->		<!-- Features Section--> */}
+<section id="gtco-features" class="bg-white">
+    <div class="container">
+        <div class="section-content">
+            {/* <!-- Section Title --> */}
+            <div class="title-wrap">
+                <h2 class="section-title">
+                    All the  <span>need </span>that you <span>want</span>
+                </h2>
+                <p class="section-sub-title">Praesent commodo cursus magna, vel scelerisque nisl consectetur et. <br/> pharetra augue. Donec id elit non mi.</p>
+            </div>
+            {/* <!-- End of Section Title --> */}
+            <div class="row">
+                {/* <!-- Features Holder--> */}
+                <div class="col-md-12 features-holder">
+                    <div class="row">
+                        {/* <!-- Features Item --> */}
+                        <div class="col-md-4 col-sm-6 feature-item item mb-3 mb-3 text-center">
+                            <div class="my-4">
+                                <i class="lnr lnr-cog fs-40"></i>
+                            </div>
+                            <h4>Easy to Customize</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        </div>
+                        {/* <!-- End of Feature Item --> */}
+                        {/* <!-- Features Item --> */}
+                        <div class="col-md-4 col-sm-6 feature-item item mb-3 text-center">
+                            <div class="my-4">
+                                <i class="lnr lnr-frame-contract fs-40"></i>
+                            </div>
+                            <h4>Pixel Perfect</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        </div>
+                        {/* <!-- End of Feature Item --> */}
+                        {/* <!-- Features Item --> */}
+                        <div class="col-md-4 col-sm-6 feature-item item mb-3 text-center">
+                            <div class="my-4">
+                                <i class="lnr lnr-bubble fs-40"></i>
+                            </div>
+                            <h4>Full Support</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        </div>
+                        {/* <!-- End of Feature Item --> */}
+                        {/* <!-- Features Item --> */}
+                        <div class="col-md-4 col-sm-6 feature-item item mb-3 text-center">
+                            <div class="my-4">
+                                <i class="lnr lnr-magic-wand fs-40"></i>
+                            </div>
+                            <h4>Clean Design</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        </div>
+                        {/* <!-- End of Feature Item --> */}
+                        {/* <!-- Features Item --> */}
+                        <div class="col-md-4 col-sm-6 feature-item item mb-3 text-center">
+                            <div class="my-4">
+                                <i class="lnr lnr-clock fs-40"></i>
+                            </div>
+                            <h4>Ontime Project</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        </div>
+                        {/* <!-- End of Feature Item --> */}
+                        {/* <!-- Features Item --> */}
+                        <div class="col-md-4 col-sm-6 feature-item item mb-3 text-center">
+                            <div class="my-4">
+                                <i class="lnr lnr-thumbs-up fs-40"></i>
+                            </div>
+                            <h4>Built with SASS</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        </div>
+                        {/* <!-- End of Feature Item --> */}
+                    </div>
+                </div>
+                {/* <!-- End of Features Holder--> */}
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+  )
+}
+export default Carousel;
